7e5315a947b5e7e4279148f5aeea44fbc6ccb92a
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2023-02-13 15:42:13 +0100

    :heavy_plus_sign: add orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'kpi_planning',
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [],
 })
